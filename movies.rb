7dc4f8f2e202f900edfaacf6c2aa09c5e04031bc
carlos <carlos.delgado@hotmail.it>
2020-01-16 21:53:06 +0100

    Creation of 'movies' exercise

--- movies.rb
+++ movies.rb
@@ -1,9 +1,10 @@
-class.Movies
-        def initialize(name, year)
-            
-        end
-    end
 
-    Movies.new
+movie1 = {name: "Star_wars_ep3", year: "2005"}
+movie2 = {name: "The_green_mile", year: "1999"}
+movie3 = {name: "Joker", year: "2019"}
 
+movies = [movie1, movie2, movie3]
 
+movies.each do |value|
+    puts "#{value[:name]} is a movie from #{value[:year]}"
+end    
